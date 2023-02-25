81d09dfb2e0c9a08a7244a0a1d8d1430f6c1d0cd
Chris Maki <c.william.developer@gmail.com>
2023-02-24 20:12:36 -0600

    Replace pool.js with updated version

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,41 +1,28 @@
-/**
-* You'll need to use environment variables in order to deploy your
-* pg-pool configuration to Heroku.
-* It will look something like this:
-**/
-
 const pg = require('pg');
-const url = require('url');
-let config = {};
+let pool;
 
+// When our app is deployed to the internet 
+// we'll use the DATABASE_URL environment variable
+// to set the connection info: web address, username/password, db name
+// eg: 
+//  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
 if (process.env.DATABASE_URL) {
-    // Heroku gives a url, not a connection object
-    // https://github.com/brianc/node-pg-pool
-    let params = url.parse(process.env.DATABASE_URL);
-    let auth = params.auth.split(':');
-
-    config = {
-        user: auth[0],
-        password: auth[1],
-        host: params.hostname,
-        port: params.port,
-        database: params.pathname.split('/')[1],
-        ssl: { rejectUnauthorized: false },
-        max: 10, // max number of clients in the pool
-        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-    };
-
-} else {
-    // only change the things on the right side of the ||
-    config = {
-        user: process.env.PG_USER || null, //env var: PGUSER
-        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
-        host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
-        port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
-        database: process.env.DATABASE_NAME || 'primeFeedback', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
-        max: 10, // max number of clients in the pool
-        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-    };
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+// When we're running this app on our own computer
+// we'll connect to the postgres database that is 
+// also running on our computer (localhost)
+else {
+    pool = new pg.Pool({
+        host: 'localhost',
+        port: 5432,
+        database: 'primeFeedback', 
+    });
 }
 
-module.exports = new pg.Pool(config);
+module.exports = pool;
